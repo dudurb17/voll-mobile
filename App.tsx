cdb8b66322e1b5513161874002d1bbcaaae4edf6
dudurb17 <eduardo.robettibedin@gmail.com>
2024-03-15 14:32:01 -0300

    mudando o statusBar

--- App.tsx
+++ App.tsx
@@ -1,6 +1,12 @@
-import { StatusBar } from "expo-status-bar";
 import Principal from "./src/Principal";
+import { NativeBaseProvider, StatusBar } from "native-base";
+import { TEMAS } from "./src/estilos/temas";
 
 export default function App() {
-  return <Principal />;
+  return (
+    <NativeBaseProvider theme={TEMAS}>
+      <StatusBar backgroundColor={TEMAS.colors.blue[800]} />
+      <Principal />
+    </NativeBaseProvider>
+  );
 }
