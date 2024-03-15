7e96ece7dbedefff54e1c200454830670ba91c53
dudurb17 <eduardo.robettibedin@gmail.com>
2024-03-15 14:27:48 -0300

    criando o tema geral

--- App.tsx
+++ App.tsx
@@ -1,20 +1,6 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import Principal from "./src/Principal";
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Hello word</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+  return <Principal />;
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
