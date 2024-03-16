51c5eb33fb35ec08265c931655253a0850e94434
dudurb17 <eduardo.robettibedin@gmail.com>
2024-03-16 18:23:37 -0300

    tela home

--- src/Tabs/Principal.tsx
+++ src/Tabs/Principal.tsx
@@ -1,17 +1,40 @@
-import { Image, ScrollView, Text } from "native-base";
+import {
+  Box,
+  Divider,
+  Image,
+  ScrollView,
+  Text,
+  VStack,
+  View,
+} from "native-base";
 import React from "react";
 import Logo from ".././assets/Logo.png";
 import { Titulo } from "../components/Titulo";
 import CardInput from "../components/CardInput";
+import { depoimentos } from "../utils/Depoimentos";
 
 const Principal = () => {
   return (
-    <ScrollView p={5}>
-      <Image mt={5} source={Logo} />
-      <Titulo color={"blue.500"} alignSelf={"flex-start"}>
-        Boas-vindas!
-      </Titulo>
-      <CardInput />
+    <ScrollView flex={1}>
+      <VStack p={5} flex={1}>
+        <Image mt={5} source={Logo} />
+        <Titulo color={"blue.500"} alignSelf={"flex-start"}>
+          Boas-vindas!
+        </Titulo>
+        <CardInput />
+        <Titulo color={"blue.800"}>Depoimentos</Titulo>
+        {depoimentos.map((item) => {
+          return (
+            <Box key={item.id}>
+              <Text textAlign={"justify"}>{item.text}</Text>
+              <Titulo fontSize={16} mt={2}>
+                {item.local}
+              </Titulo>
+              <Divider mb={2} />
+            </Box>
+          );
+        })}
+      </VStack>
     </ScrollView>
   );
 };
---
+++ src/utils/Depoimentos.ts
@@ -0,0 +1,17 @@
+export const depoimentos = [
+    {
+      id: 1,
+      text: `At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id estlaborum et dolorum fuga. Et harum qurerum facilis est et expeditadistinctio.`,
+      local: "Júlio, 40 anos, São Paulo/SP.",
+    },
+    {
+      id: 2,
+      text: `Nam libero tempore, cum soluta nobis est eligendi optio cumque nihil impedit quo minus id quod maxime placeat facere possimus, omnis voluptas assumenda est, omnis dolor repellendus.`,
+      local: "Gabriel, 40 anos, São Paulo/SP.",
+    },
+    {
+      id: 3,
+      text: `Temporibus autem quibusdam et aut officiis debitis aut rerum necessitatibus saepe eveniet ut et voluptates repudiandae sint et molestiae non recusandae. Itaque earum rerum hic tenetur a sapiente delectus, ut aut reiciendis voluptatibus maiores alias consequatur aut perferendis doloribus asperiores`,
+      local: "Paulo, 40 anos, São Paulo/SP.",
+    },
+  ];
