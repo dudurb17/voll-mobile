bc58e17bc801475bb21e42369e583cbe8de98bb1
dudurb17 <eduardo.robettibedin@gmail.com>
2024-03-16 17:37:55 -0300

    Criando component de card

--- src/Tabs/Consultas.tsx
+++ src/Tabs/Consultas.tsx
@@ -1,8 +1,21 @@
-import { Text } from "native-base";
+import { Text, VStack } from "native-base";
 import React from "react";
+import CardConsulta from "../components/CardConsulta";
 
 const Consultas = () => {
-  return <Text>Consultas</Text>;
+  return (
+    <VStack p={5}>
+      <Text>Consultas</Text>
+      <CardConsulta
+        name="Eduardo"
+        especialidade="Cardiologista"
+        img="https://avatars.githubusercontent.com/u/114515828?v=4"
+        data="20/05/2005"
+        foiAfendado
+        foiAtendido
+      />
+    </VStack>
+  );
 };
 
 export default Consultas;
---
+++ src/components/CardConsulta.tsx
@@ -0,0 +1,52 @@
+import { Avatar, Text, VStack } from "native-base";
+import React from "react";
+import { Botao } from "./Botao";
+
+interface Props {
+  name: string;
+  img: string;
+  especialidade: string;
+  data?: string;
+  foiAtendido?: boolean;
+  foiAfendado?: boolean;
+}
+
+const CardConsulta = ({
+  name,
+  img,
+  especialidade,
+  data,
+  foiAfendado,
+  foiAtendido,
+}: Props) => {
+  return (
+    <VStack
+      w="100%"
+      bg={foiAtendido ? "blue.100" : "white"}
+      p={5}
+      borderRadius={"lg"}
+      shadow={2}
+    >
+      <VStack flexDirection={"row"}>
+        <Avatar
+          size={"lg"}
+          source={{
+            uri: img,
+          }}
+        />
+        <VStack pl={4}>
+          <Text fontSize={"md"} bold>
+            {name}
+          </Text>
+          <Text>{especialidade}</Text>
+          <Text>{data}</Text>
+        </VStack>
+      </VStack>
+      <Botao mt={4}>
+        {foiAfendado ? "Cancelar consulta" : "Agendar consulta"}
+      </Botao>
+    </VStack>
+  );
+};
+
+export default CardConsulta;
