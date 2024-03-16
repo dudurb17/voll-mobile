c741f2bfe21c0d4433019fe6ece3880ef33aa5aa
dudurb17 <eduardo.robettibedin@gmail.com>
2024-03-16 13:26:05 -0300

    criando tela perfil

--- src/Tabs/Perfil.tsx
+++ src/Tabs/Perfil.tsx
@@ -1,8 +1,34 @@
-import { Text } from "native-base";
+import { Text, VStack, ScrollView, Avatar, Divider } from "native-base";
 import React from "react";
+import { Titulo } from "../components/Titulo";
 
 const Perfil = () => {
-  return <Text>Perfil</Text>;
+  return (
+    <ScrollView flex={1}>
+      <VStack flex={1} alignItems={"center"} p={5}>
+        <Titulo color={"blue.500"}>Meu Perfil</Titulo>
+        <Avatar
+          size={"xl"}
+          source={{
+            uri: "https://avatars.githubusercontent.com/u/114515828?v=4",
+          }}
+          mt={5}
+        />
+        <Titulo color={"blue.500"}>Informações pessoais</Titulo>
+        <Titulo fontSize="lg" mb={1}>
+          Eduardo Bedin
+        </Titulo>
+        <Text>24/01/2005</Text>
+        <Text>Santa Catarina</Text>
+        <Divider />
+        <Titulo color={"blue.500"} mb={1}>
+          Histórico médico
+        </Titulo>
+        <Text>Bronquite</Text>
+        <Text>Sinusite</Text>
+      </VStack>
+    </ScrollView>
+  );
 };
 
 export default Perfil;
