aa77a79c4f571906c1299b875fac4bee94a81fe1
dudurb17 <eduardo.robettibedin@gmail.com>
2024-03-16 18:35:29 -0300

    criandoo pesquisa e deixando o cod limpo

--- src/Tabs/Explorar.tsx
+++ src/Tabs/Explorar.tsx
@@ -1,8 +1,30 @@
-import { Text } from "native-base";
+import { ScrollView, Text, VStack } from "native-base";
 import React from "react";
+import CardInput from "../components/CardInput";
+import { Titulo } from "../components/Titulo";
+import CardConsulta from "../components/CardConsulta";
+import { data } from "../utils/Data";
 
 const Explorar = () => {
-  return <Text>Explorar</Text>;
+  return (
+    <ScrollView flex={1}>
+      <VStack p={5} flex={1}>
+        <CardInput />
+        <Titulo color={"blue.500"}>Resultado da busca</Titulo>
+
+        {data.map((item) => {
+          return (
+            <CardConsulta
+              name={item.name}
+              img={item.img}
+              especialidade={item.especialidade}
+              key={item.id}
+            />
+          );
+        })}
+      </VStack>
+    </ScrollView>
+  );
 };
 
 export default Explorar;
---
+++ src/utils/Data.ts
@@ -0,0 +1,27 @@
+export const data = [
+    {
+      id: 1,
+      name: "Dra. Ana Lúcia",
+      img: "https://www.boaconsulta.com/blog/wp-content/uploads/2019/04/como-fazer-a-marcacao-de-consultas-online-confira-o-passo-a-passo.jpeg",
+      especialidade: "Angiologista",
+    },
+    {
+      id: 2,
+      name: "Dra. Paula Lúcia",
+      img: "https://www.boaconsulta.com/blog/wp-content/uploads/2019/04/como-fazer-a-marcacao-de-consultas-online-confira-o-passo-a-passo.jpeg",
+      especialidade: "Rim",
+    },
+    {
+      id: 3,
+      name: "Dra. Roberta Lúcia",
+      img: "https://www.boaconsulta.com/blog/wp-content/uploads/2019/04/como-fazer-a-marcacao-de-consultas-online-confira-o-passo-a-passo.jpeg",
+      especialidade: "medica",
+    },
+    {
+      id: 4,
+      name: "Dra. Angelica Lúcia",
+      img: "https://www.boaconsulta.com/blog/wp-content/uploads/2019/04/como-fazer-a-marcacao-de-consultas-online-confira-o-passo-a-passo.jpeg",
+      especialidade: "Pulmão",
+    },
+  ];
+  
